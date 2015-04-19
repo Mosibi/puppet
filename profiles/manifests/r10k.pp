5977737ec5931fccd64757a432e7cdb06392cfd4
Richard Arends <github@mosibi.nl>
2015-04-19 21:14:18 +0200

    added basedir param

--- profiles/manifests/r10k.pp
+++ profiles/manifests/r10k.pp
@@ -1,8 +1,8 @@
 class profiles::r10k {
   class { 'r10k':
-    remote   => 'https://github.com/Mosibi/puppet.git',
-    version  => '1.5.1',
-    provider => 'puppet_gem',
-#    require  => File['gemrc'],
+    r10k_basedir => '/etc/puppetlabs/code',
+    remote       => 'https://github.com/Mosibi/puppet.git',
+    version      => '1.5.1',
+    provider     => 'puppet_gem',
   }
 }
