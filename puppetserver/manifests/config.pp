29d333671700744553ce48cfe6a70e74cecb5636
Richard Arends <github@mosibi.nl>
2015-04-19 20:36:15 +0200

    first commit

---
+++ puppetserver/manifests/config.pp
@@ -0,0 +1,46 @@
+class puppetserver::config inherits ::puppetserver {
+ file {
+    [
+      '/etc/puppetlabs/code/hieradata/environments',
+    ]:
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  file { '/etc/default/puppetserver':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('puppetserver/etc/default/puppetserver.erb'),
+    notify  => Class['::puppetserver::service'],
+  }
+
+  file { '/etc/puppetlabs/code/hiera.yaml':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('puppetserver/etc/puppetlabs/code/hiera.yaml.erb'),
+  }
+
+  if ! defined(File['/etc/puppetlabs/puppet/puppet.conf']) { 
+    file { '/etc/puppetlabs/puppet/puppet.conf':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('puppetserver/etc/puppetlabs/puppet/puppet.conf.erb'),
+      notify  => Class['::puppetserver::service'],
+    }
+  }
+
+
+##file { '/etc/puppet/manifests/site.pp':
+##  owner   => 'root',
+##  group   => 'root',
+##  mode    => '0644',
+##  source  => 'puppet:///modules/puppetserver/etc/puppet/manifests/site.pp',
+##  require => Class['puppetserver::install']
+##}
+
+}
