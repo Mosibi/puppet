f0570d07d5fe8f920be640b497ef7bda48c792ae
Richard Arends <github@mosibi.nl>
2015-04-19 21:23:44 +0200

    fix basedir

--- profiles/manifests/r10k.pp
+++ profiles/manifests/r10k.pp
@@ -1,6 +1,6 @@
 class profiles::r10k {
   class { 'r10k':
-    r10k_basedir => '/etc/puppetlabs/code',
+    r10k_basedir => '/etc/puppetlabs/code/environments',
     remote       => 'https://github.com/Mosibi/puppet.git',
     version      => '1.5.1',
     provider     => 'puppet_gem',
